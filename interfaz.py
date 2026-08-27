26be3af5e6ed8c5136ddcf41299024f844c30025
Alexet201 <alejandroesau396@gmail.com>
2026-08-27 08:24:57 -0600

    graafica

--- interfaz.py
+++ interfaz.py
@@ -1,6 +1,35 @@
 import tkinter as tt
+import matplotlib.pyplot as plt
+
+x = [1, 2, 3, 4, 5]
+y = [2, 4, 6, 8, 10]
+
+#plt.plot (x, y)
+plt.title("Gráfica de ejemplo")
+#plt.bar(x, y)
+plt.scatter(x, y)
+plt.xlabel("Eje X")
+plt.ylabel("Eje Y")
+plt.show()
+
+'''
+def saludar (): 
+    nombre = entry.get().strip()
+    if not nombre:
+        nombre = "Alejandro"
+    lbl.config(text=f"Hola {nombre}")
 
 root = tt.Tk()
 root.title("Saludar")
 root.geometry("360x220")
+lbl = tt.Label(root, text="Ingrese su nombre", background= "blue" , foreground="black", font=("Arial", 12))
+lbl.pack (pady = 20)
+entry = tt.Entry(root, background= "aqua")
+entry.pack(pady=10)
+bot = tt.Button(root, text="Mostrar", background="lightgray", command=saludar)
+bot.pack(pady=10)
+
+
+
 root.mainloop()
+'''
